b6ed8675fb09a107c2cf953bf363a15bb28f517c
Accu <matthewsoto85@gmail.com>
2024-01-07 17:16:00 -0500

    starter code for pacman

--- game.py
+++ game.py
@@ -1,1 +1,20 @@
-print('Hello World')
+import pygame
+
+pygame.init()
+
+# Dimensions
+WIDTH,HEIGHT = 1200,950
+screen = pygame.display.set_mode([WIDTH,HEIGHT])
+
+game_run = True
+while game_run:
+    #sets up the screen color
+    screen.fill((0,0,0))
+
+    #checking for if we are going to end the game so it doesn't run infinite
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_run = False
+    pygame.display.flip()
+
+pygame.quit()
